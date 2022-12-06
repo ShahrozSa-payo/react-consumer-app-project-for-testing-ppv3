36d483f1ee2a2641614128194ddafc9e97938874
Shahroz Saleem <shahrozsa@payoneer.com>
2022-12-06 18:04:21 +0500

    ppv3 version info consumer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { OP_PAYMENT_WIDGET_V3_VERSION } from "test-with-pkg-version-for-pi-payment-widget";
+import pkg from "../package.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        PPv3 Package version from ESM package import is {" "}
+        <span style={{color: "red"}}>{OP_PAYMENT_WIDGET_V3_VERSION}</span>
+      </h2>
+
+      <h2>
+        React App version is {" "}
+        <span style={{color: "red"}}>{pkg.version}</span>
+      </h2>
     </div>
   );
 }
 
 export default App;
+
